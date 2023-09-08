0a02acc2fc100a0b79d97ee541bd8fa7e66fc68d
Sebastien Roche <sroche@g.harvard.edu>
2023-09-08 15:58:54 -0400

    changed gcloud_dset to msat_dset and allow using regular paths or blobs

--- msatutil/msat_gcloud.py
+++ msatutil/msat_gcloud.py
@@ -1,17 +1,35 @@
 from google.cloud import storage
 from google.cloud.storage.blob import Blob
+from google.cloud.storage.client import Client
 from netCDF4 import Dataset
+from typing import Union
 
 
-class cloud_dset(Dataset):
+class msat_dset(Dataset):
     """
     Class to open a netcdf file on a google cloud bucket
     Download the file in memory and produce a netCDF4._netCDF4.Dataset object
     """
-    def __init__(self, cloud_file_path, client=None):
-        if client is None:
-            client = storage.Client()
-        blob = Blob.from_string(cloud_file_path, client=client)
-        with blob.open("rb") as infile:
-            data = infile.read()
-        super().__init__("memory", memory=data)
+
+    def __init__(self, nc_target: Union[str, Blob], client: Client = None):
+        """
+        nc_target can be:
+            - a netcdf file path (str)
+            - a google cloud file path starting with gs:// (str)
+            - a google cloud blob (Blob)
+
+        client (Client): used when nc_target is a blob, defaults to storage.Client()
+        """
+        if "blob" in str(type(nc_target)).lower() or nc_target.startswith("gs://"):
+            if nc_target.startswith("gs://"):
+                if client is None:
+                    client = storage.Client()
+                nc_target = Blob.from_string(nc_target, client=client)
+            with nc_target.open("rb") as gcloud_file:
+                data = gcloud_file.read()
+            filename = "memory"
+        else:
+            data = None
+            filename = nc_target
+
+        super().__init__(filename, memory=data)
