d8f6b26e29b08541399fe34c61bc415d6610fb92
Sebastien Roche <sroche@g.harvard.edu>
2023-11-08 13:13:06 -0500

    only write the version in __init__.py, updated setup.py to parse __init__.py for the version and create tags with the git hash

--- msatutil/__init__.py
+++ msatutil/__init__.py
@@ -1,3 +1,1 @@
-# be sure to update in setup.py as well
-# please follow semantic versioning: https://semver.org/
 __version__ = '3.14.3'
--- setup.py
+++ setup.py
@@ -1,7 +1,36 @@
 import os
+import re
+import subprocess
 from setuptools import setup, find_packages
 
+
 _mydir = os.path.dirname(__file__)
+
+# parse version number from msatutil/__init__.py
+version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+with open(os.path.join(_mydir, "msatutil", "__init__.py")) as f:
+    content = f.read()
+re_search = re.search(version_re, content, re.M)
+if re_search:
+    version_str = re_search.group(1)
+else:
+    raise RuntimeError("Could not parse version string from __init__.py")
+
+# add the git hash to version_str
+if os.path.exists(os.path.join(_mydir, ".git")):
+    git_hash = subprocess.check_output(
+        "git rev-parse --verify --short HEAD", cwd=_mydir, text=True, shell=True
+    ).strip()
+    git_version_str = f"v{version_str}"
+    tags = subprocess.check_output("git tag", cwd=_mydir, text=True, shell=True)
+    if git_version_str not in tags:
+        subprocess.check_output(
+            f"git tag -a {git_version_str} {git_hash} -m 'tagged by setup.py to {version_str}'",
+            cwd=_mydir,
+            text=True,
+            shell=True,
+        )
+    version_str = f"{version_str}+git.{git_hash}"
 
 # extra dependencies for mair_geoviews.ipynb
 extras = {
@@ -20,7 +49,7 @@ setup(
     description="Utility codes to read and plot from MethaneSAT/AIR files",
     author="Sebastien Roche",
     author_email="sroche@g.harvard.edu",
-    version="3.14.3",  # make sure stays in sync with the version in msatutil/__init__.py
+    version=version_str,
     url="https://github.com/rocheseb/msatutil",
     classifiers=[
         "Operating System :: OS Independent",
@@ -36,7 +65,7 @@ setup(
         "scipy",
         "tqdm",
         "google-cloud-storage",
-        ],
+    ],
     extras_require=extras,
     packages=find_packages(),
     include_package_data=True,
