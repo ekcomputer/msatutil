09eb4304cdc577d0c270a3de3ff6bf25256417df
ekcomputer <ethan_kyzivat@fas.harvard.edu>
2024-02-21 09:41:04 -0500

    Should work on GCS- time to test

--- msatutil/make_spatial_index.py
+++ msatutil/make_spatial_index.py
@@ -1,5 +1,6 @@
 import os
-from msatutil.msat_dset import msat_dset
+import platform
+from msat_dset import msat_dset
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -9,20 +10,43 @@ from shapely.geometry import Polygon, MultiPolygon, shape
 from rasterio.transform import from_origin
 
 ## I/O
-L3_mosaics_catalogue_pth = '/Volumes/metis/MAIR/Index/L3_mosaics.xlsx'
-working_dir = '/Volumes/metis/MAIR/Spatial_catalogue'
-catalogue_shp_out_pth = os.path.join(working_dir, 'L3_mosaics_20240208.shp')
-
-## I/O
-L3_mosaics_catalogue_pth = '/Volumes/metis/MAIR/Index/L3_mosaics.xlsx'
-working_dir = '/Volumes/metis/MAIR/Spatial_catalogue'
-catalogue_shp_out_pth = os.path.join(working_dir, 'L3_mosaics_20240208.shp')
+# L3_mosaics_catalogue_pth = '/Volumes/metis/MAIR/Index/L3_mosaics.xlsx'
+L3_mosaics_catalogue_pth = 'gs://msat-dev-science-data/L3_mosaics.csv'
+os_platform = platform.platform()
 
 ## Load
-gdf = pd.read_excel(L3_mosaics_catalogue_pth)
+# gdf = pd.read_excel(L3_mosaics_catalogue_pth)
+if os_platform == 'macOS-14.2.1-arm64-arm-64bit':
+    df = pd.read_csv(L3_mosaics_catalogue_pth, storage_options={
+        'token': '~/.config/gcloud/application_default_credentials_MAIR.json'})
+    df = df[:3]  # Testing
+    working_dir = '/Volumes/metis/MAIR/Spatial_catalogue'
+else:  # on GCS
+    df = pd.read_csv(L3_mosaics_catalogue_pth,
+                     storage_options={'token': 'cloud'})
+    working_dir = '~/msat_spatial_idx'
+catalogue_shp_out_pth = os.path.join(working_dir, 'L3_mosaics_20240208.shp')
 
+## TODO: problem is google-auth installation is not working. (Can't import gcsfs). I had google-auth 1.35 installed, then installed 2.28 with pip, but it had a dependency conflict, so I uninstalled. Tried to install with mamba, but it tells me it is already installed, even though import google-auth fails...
+
+## Could also try loading Sebastian's way and using pandas to load 'data'
+'''
+
+e.g. with nc_target.open("rb") as gcloud_file:
+                data = gcloud_file.read()
+
+    +google-api-core-1.31.5 (conda-forge/noarch)
+    +google-api-python-client-1.12.8 (conda-forge/noarch)
+    +google-auth-1.35.0 (conda-forge/noarch)
+    +google-auth-httplib2-0.1.0 (conda-forge/noarch)
+    +google-cloud-core-2.3.1 (conda-forge/noarch)
+    +google-cloud-storage-2.10.0 (conda-forge/noarch)
+    +google-crc32c-1.1.2 (conda-forge/osx-arm64)
+    +google-resumable-media-2.5.0 (conda-forge/noarch)
+    +googleapis-common-protos-1.60.0 (conda-forge/noarch)
+
+'''
 ## Functions
-
 
 def validDataArea2Gdf(ds, simplify=None):
     '''
@@ -78,19 +102,20 @@ def validDataArea2Gdf(ds, simplify=None):
 
 
 if __name__ == '__main__':
-    gdf = gdf[:3]  # Testing
-    gdf
-    for index, row in gdf.iterrows():
+    for index, row in df.iterrows():
         gs_pth = row['uri']
         print(gs_pth.split('/mosaic/')[-1])
         ds = msat_dset(gs_pth)
         geom = validDataArea2Gdf(ds, simplify=0.001)
-        gdf.at[index, 'geometry'] = geom
+        df.at[index, 'geometry'] = geom
 
-    gdf
-    gdf.crs = 'EPSG:4326'
-    gdf = gpd.GeoDataFrame(gdf, geometry='geometry',
-                           crs='EPSG:4326')  # , crs='EPSG:4326')
+    gdf = gpd.GeoDataFrame(df, geometry='geometry',
+                           crs='EPSG:4326')
+
+    ## ESRI shapefile can't handle datetimeformat
     for col in ['flight_date', 'production_timestamp', 'time_start', 'time_end']:
         gdf[col] = gdf[col].astype(str)
+
+    ## Save as shapefile and geojson to disk
     gdf.to_file(catalogue_shp_out_pth)
+    gdf.to_file(catalogue_shp_out_pth.replace('.shp', '.geojson'))
