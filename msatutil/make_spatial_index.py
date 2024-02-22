f5fb76ac8e4f6dd8d463f6b43d9dfc4cbc8f5731
Ethan Kyzivat on GCP <ethan_kyzivat@g.harvard.edu>
2024-02-22 16:18:32 +0000

    Final working version with tolerance switch.

--- msatutil/make_spatial_index.py
+++ msatutil/make_spatial_index.py
@@ -12,37 +12,12 @@ from rasterio.transform import from_origin
 TODO:
 * Fix if branches to make less redundant
 '''
-## I/O
-# L3_mosaics_catalogue_pth = '/Volumes/metis/MAIR/Index/L3_mosaics.xlsx'
-L3_mosaics_catalogue_pth = 'gs://msat-dev-science-data/L3_mosaics.csv'
-os_platform = platform.platform()
-load_from_chkpt = True
-
-## Load
-# gdf = pd.read_excel(L3_mosaics_catalogue_pth)
-if os_platform == 'macOS-14.2.1-arm64-arm-64bit':
-    df = pd.read_csv(L3_mosaics_catalogue_pth)  # , storage_options={
-    # 'token': '~/.config/gcloud/application_default_credentials_MAIR.json'})
-    df = df[:3]  # Testing
-    working_dir = '/Volumes/metis/MAIR/Spatial_catalogue'
-else:  # on GCS 'Linux-6.2.0-1013-gcp-x86_64-with-glibc2.35'
-    working_dir = '~/msat_spatial_idx'
-    if load_from_chkpt:
-        catalogue_tmp_out_pth = os.path.join(  # sloppy duplicate line
-            working_dir, 'L3_mosaics_20240208_tmp.csv')
-        df = pd.read_csv(catalogue_tmp_out_pth)
-    else:
-        df = pd.read_csv(L3_mosaics_catalogue_pth,
-                         storage_options={'token': 'cloud'})
-catalogue_shp_out_pth = os.path.join(working_dir, 'L3_mosaics_20240208.shp')
-catalogue_tmp_out_pth = os.path.join(
-    working_dir, 'L3_mosaics_20240208_tmp.csv')
 
 ## Functions
 
 def validDataArea2Gdf(ds, simplify=None):
     '''
-    This function converts valid data areas from a msatutil.msat_dset.msat_dset (Netcdf4 Dataset) into a MultiPolygon Shapefly geometry.
+    This function converts valid data areas from a msatutil.msat_dset.msat_dset (Netcdf4 Dataset) into a MultiPolygon Shapefly geometry. It takes about an hour to run for 1,000 mosaics.
 
     Parameters:
     - ds: Netcdf4 Dataset object containing the data
@@ -52,11 +27,12 @@ def validDataArea2Gdf(ds, simplify=None):
     - multipolygon: A MultiPolygon object representing the valid data areas
 
     Notes:
-    - The function assumes a specific geospatial resolution of '1/3 arcseconds' and returns an assertion error if this is not true.
+    - The function assumes a specific geospatial resolution of 1/3 or 1arcseconds and returns an assertion error if this is not true.
     - The function assumes geographic coordinates in WGS84 (EPSG:4326).
     - The function assumes that the dataset has a 'xch4' variable that contains the valid data areas.
     - It creates a MultiPolygon object from valid data areas based on the 'xch4' variable in the dataset.
     - The function uses a transformation defined by the geospatial information of the dataset.
+    - There were some errors using a simplify_tol of 0.001 degrees. Using 0.01 instead is slightly too coarse, but saves disk space and makes rendering quick.
 
     Examples:
     validDataArea2Gdf(ds, simplify=None)
@@ -94,30 +70,68 @@ def validDataArea2Gdf(ds, simplify=None):
 
 
 if __name__ == '__main__':
+    ## I/O
+    # L3_mosaics_catalogue_pth = '/Volumes/metis/MAIR/Index/L3_mosaics.xlsx'
+    L3_mosaics_catalogue_pth = 'gs://msat-dev-science-data/L3_mosaics.csv'
+    os_platform = platform.platform()
+    load_from_chkpt = True
+    simplify_tol = 0.01 # in map units (deg)
+
+    ## Load
+    # gdf = pd.read_excel(L3_mosaics_catalogue_pth)
+    if simplify_tol is not None:
+        tol_str = f'_tol{simplify_tol}'
+    else:
+        tol_str = ''
+    if os_platform == 'macOS-14.2.1-arm64-arm-64bit':
+        working_dir = '/Volumes/metis/MAIR/Spatial_catalogue' 
+        catalogue_shp_out_pth = os.path.join(working_dir, f'L3_mosaics_20240208{tol_str}.shp')
+        if load_from_chkpt and os.path.exists(os.path.expanduser(catalogue_shp_out_pth)):
+            df = gpd.read_file(catalogue_shp_out_pth, driver='ESRI Shape')
+        else:
+            df = pd.read_csv(L3_mosaics_catalogue_pth)            
+        df = df[:3]  # Testing
+    else:  # on GCS 'Linux-6.2.0-1013-gcp-x86_64-with-glibc2.35'
+        working_dir = '~/msat_spatial_idx'
+        catalogue_shp_out_pth = os.path.join(working_dir, f'L3_mosaics_20240208{tol_str}.shp')
+        if load_from_chkpt and os.path.exists(os.path.expanduser(catalogue_shp_out_pth)):
+            df = gpd.read_file(catalogue_shp_out_pth, driver='ESRI Shape')
+        else:
+            df = pd.read_csv(L3_mosaics_catalogue_pth,
+                            storage_options={'token': 'cloud'})
+
+    ## Loop
     for index, row in df.iterrows():
         gs_pth = row['uri']
         if 'geometry' in df.columns:  # loaded from checkpoint
-            if not np.isnan(df.at[index, 'geometry']):
+            if  df.at[index, 'geometry'] is not None: #isinstance(df.at[index, 'geometry'], str):
                 print(f"Geometry exists for {gs_pth.split('/mosaic/')[-1]}")
                 continue  # Skip if geometry is already present
 
-        print(gs_pth.split('/mosaic/')[-1])
+        print(f"[{index}] {gs_pth.split('/mosaic/')[-1]}")
         ds = msat_dset(gs_pth)
-        geom = validDataArea2Gdf(ds, simplify=0.001)
+        geom = validDataArea2Gdf(ds, simplify=simplify_tol)
         df.at[index, 'geometry'] = geom
 
-        ## Save temporarily
-        if index % 10 == 0:
-            df.to_csv(catalogue_tmp_out_pth)
-            print('\t> Saving checkpoint.')
+        ## Save intermittently
+        if (index % 50 == 0) or index == len(df):
+            # df.to_csv(catalogue_tmp_out_pth)
+            if index % 10 == 0:
+                print('\t> Saving checkpoint.')
+            if index == len(df):
+                print('\t> Saving final.')
+            gdf = gpd.GeoDataFrame(df, geometry='geometry',
+                                crs='EPSG:4326')
 
-    gdf = gpd.GeoDataFrame(df, geometry='geometry',
-                           crs='EPSG:4326')
-
-    ## ESRI shapefile can't handle datetimeformat
-    for col in ['flight_date', 'production_timestamp', 'time_start', 'time_end']:
-        gdf[col] = gdf[col].astype(str)
-
-    ## Save as shapefile and geojson to disk
-    gdf.to_file(catalogue_shp_out_pth)
-    gdf.to_file(catalogue_shp_out_pth.replace('.shp', '.geojson'))
+            ## ESRI shapefile can't handle datetimeformat
+            try:
+                for col in ['flight_date', 'production_timestamp', 'time_start', 'time_end']:
+                    gdf[col] = gdf[col].astype(str)
+            except:
+                for col in ['flight_dat', 'producti_2', 'time_start', 'time_end']:
+                    gdf[col] = gdf[col].astype(str)
+            
+            ## Save as shapefile and geojson to disk
+            gdf.to_file(catalogue_shp_out_pth, driver='ESRI Shapefile')
+            gdf.to_file(catalogue_shp_out_pth.replace('.shp', '.geojson'))
+    print('Finished creating mask shapefile.')
